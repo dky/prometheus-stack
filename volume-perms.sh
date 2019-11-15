c20f6c6073d6a3dc959f042d5d8f664de4ff7d1d
Don Ky <don.d.ky@gmail.com>
2019-11-15 17:10:01 -0500

    Add volume perms

---
+++ volume-perms.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This script fixes the file permissions volume data + configs to match what's inside the container.
+
+GRAFANA_ID=`docker run --rm --entrypoint "/usr/bin/id" grafana/grafana "-u"`
+GRAFANA_DATA_VOLUME=./data/grafana
+echo "Grafana UID is: $GRAFANA_ID"
+sudo chown -R $GRAFANA_ID:$GRAFANA_ID $GRAFANA_DATA_VOLUME
+
+PROM_ID=`docker run --rm --entrypoint "/bin/id" prom/prometheus:v2.4.0 "-u"`
+PROM_DATA_VOLUME=./data/prometheus
+echo "Prometheus UID is: $PROM_ID"
+sudo chown -R $PROM_ID:$PROM_ID $PROM_DATA_VOLUME
